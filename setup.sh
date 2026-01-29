c29a454609763b331c7f34c4d2261394fc2f8b6b
Loris Bognanni <loris@codemade.co.uk>
2026-01-29 20:01:54 +0000

    :recycle: download C dependencies instead of including them

--- setup.sh
+++ setup.sh
@@ -6,8 +6,29 @@ RAYLIB_URL="https://github.com/raysan5/raylib/releases/download/${RAYLIB_VERSION
 LIB_DIR="lib"
 RAYLIB_DIR="${LIB_DIR}/raylib-${RAYLIB_VERSION}_linux_amd64"
 
+STB_BASE_URL="https://raw.githubusercontent.com/nothings/stb/refs/heads/master"
+STB_IMAGE_RESIZE_URL="${STB_BASE_URL}/stb_image_resize2.h"
+STB_IMAGE_URL="${STB_BASE_URL}/stb_image.h"
+INCLUDE_DIR="include"
+
 echo "FastTab Development Setup"
 echo "========================="
+
+if [ ! -d "$INCLUDE_DIR" ]; then
+    mkdir -p "$INCLUDE_DIR"
+fi
+
+# Download stb_image_resize.h
+if [ ! -f "${INCLUDE_DIR}/stb_image_resize2.h" ]; then
+    echo "Downloading stb_image_resize2.h..."
+    curl -L "$STB_IMAGE_RESIZE_URL" -o "${INCLUDE_DIR}/stb_image_resize2.h"
+fi
+
+# Download stb_image.h
+if [ ! -f "${INCLUDE_DIR}/stb_image.h" ]; then
+    echo "Downloading stb_image.h..."
+    curl -L "$STB_IMAGE_URL" -o "${INCLUDE_DIR}/stb_image.h"
+fi
 
 # Check if raylib is already set up
 if [ -d "$RAYLIB_DIR" ]; then
